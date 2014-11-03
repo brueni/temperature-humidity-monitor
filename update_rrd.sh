de30259abd4298572893a337b84c65cf5669926b
brueni <info@michael-bruenisholz.ch>
2014-11-03 22:23:04 +0100

    Made the script directory-independent, so it works with cron

--- update_rrd.sh
+++ update_rrd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd `dirname $0`
 #Execute every 300sec to update values from python-script to RRD
 #Store Output from python file in 'output' variable
 output=$(python get_sensor_values.py 11 4)
