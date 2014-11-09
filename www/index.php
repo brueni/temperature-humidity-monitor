1e3474554ff768dc56f7d9b75f30eac2e6abd657
brueni <info@michael-bruenisholz.ch>
2014-11-09 16:38:48 +0100

    Added Navigation

--- www/index.php
+++ www/index.php
@@ -3,79 +3,80 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>Highcharts Example</title>
-		
-		
-		<!-- 1. Add these JavaScript inclusions in the head of your page -->
+		<title>Temperature and Humidity</title>
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script type="text/javascript" src="http://code.highcharts.com/highcharts.js"></script>
-		
-		
-		<!-- 2. Add the JavaScript to initialize the chart on document ready -->
-		<script type="text/javascript">
-		$(document).ready(function() {
-			
 
-			var options = {
-				chart: {
-					renderTo: 'container',
-					defaultSeriesType: 'column'
-				},
-				title: {
-					text: 'Temperature'
-				},
-				xAxis: {
-					categories: []
-				},
-				yAxis: {
-					title: {
-						text: 'Deg C'
-					}
-				},
-				series: []
-			};
-			
-			// Load the data from the XML file 
-			$.get('temperature3h.xml', function(xml) {
-				
-				// Split the lines
-				var $xml = $(xml);
-				
-				// push categories
-				//$xml.find('categories item').each(function(i, category) {
-				//	options.xAxis.categories.push($(category).text());
-				//});
-				
-				// push series
-				$xml.find('data').each(function(i, series) {
-					var seriesOptions = {
-						name: $(series).find('name').text(),
-						data: []
-					};
-					
-					// push data points
-					$(series).find('row v').each(function(i, point) {
-						seriesOptions.data.push(
-							parseFloat($(point).text())
-						);
-					
-					// add it to the options
-					options.series.push(seriesOptions);
-				});
-				var chart = new Highcharts.Chart(options);
-			});
-			
-			
-		});
+		<script src="functions.js" type="text/javascript"></script>   
+		<script type="text/javascript">
+
+		$.ajax({
+		    type: "GET",
+		    url: "humid1d.xml",
+		    dataType: "xml",
+		    success: function(xml) {
+		        var series = []
+
+		        //define series
+		        $(xml).find("entry").each(function() {
+		            var seriesOptions = {
+		                name: $(this).text(),
+		                data: []
+		            };
+		            options.series.push(seriesOptions);
+		        });
+
+		        //populate with data
+		        $(xml).find("row").each(function() {
+		            var t = parseInt($(this).find("t").text()) * 1000
+
+		            $(this).find("v").each(function(index) {
+		                var v = parseFloat($(this).text())
+		                v = v || null
+		                if (v != null) {
+		                    options.series[index].data.push([t, v])
+		                };
+		            });
+		        });
+		 
+		        options.title.text = "Temperatures of the last xyh"
+		        $.each(series, function(index) {
+		            options.series.push(series[index]);
+		        });
+		        chart = new Highcharts.Chart(options);
+		    }
+		})
 		</script>
-		
+
 	</head>
 	<body>
-		
-		<!-- 3. Add the container -->
-		<div id="container" style="width: 800px; height: 400px; margin: 0 auto"></div>
-		
-				
+		<div id="site">
+			<div id="navigation" style="width: 20%; heigth:400px; margin: 0 auto">
+				<b>Temperatures</b><br>
+				<ul>
+					<li><a href="?type=t&range=3h">Last 3h</a></li>
+					<li>Last 24h</li>
+					<li>Last 48h</li>
+					<li>Last 1w</li>
+					<li>Last 1m</li>
+					<li>Last 3m</li>
+					<li>Last 1y</li>
+				</ul>
+				<b>Humidity</b><br>
+                                <ul>
+                                        <li>Last 3h</li>
+                                        <li>Last 24h</li>
+                                        <li>Last 48h</li>
+                                        <li>Last 1w</li>
+                                        <li>Last 1m</li>
+                                        <li>Last 3m</li>
+                                        <li>Last 1y</li>
+                                </ul>
+
+			</div>
+			<div id="container" style="width: 80%; height: 400px; margin: 0 auto"></div>
+		</div>
+
 	</body>
 </html>
 
