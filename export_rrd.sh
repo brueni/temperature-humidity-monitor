78def72dd9a53e5d70ee73f7bf1e75cd65bfe856
brueni <info@michael-bruenisholz.ch>
2014-11-09 15:53:45 +0100

    added more xml-variants, and changed output to www-dir

--- export_rrd.sh
+++ export_rrd.sh
@@ -2,11 +2,15 @@
 
 #exports the data in the RRD to an xml, which can pe parsed by highcharts
 
-rrdtool xport -s now-3h -e now --step 300 \
+rrdtool xport -s now-1d -e now --step 900 \
 DEF:a=temp-humid.rrd:temp:AVERAGE \
-XPORT:a:"Testraum" > export/temperature3h.xml
+XPORT:a:"Testraum" > www/temperature1d.xml
 
 rrdtool xport -s now-3h -e now --step 300 \
+DEF:a=temp-humid.rrd:temp:AVERAGE \
+XPORT:a:"Testraum" > www/temperature3h.xml
+
+rrdtool xport -s now-1d -e now --step 300 \
 DEF:a=temp-humid.rrd:humid:AVERAGE \
-XPORT:a:"Testraum" > export/humid3h.xml
+XPORT:a:"Testraum" > www/humid1d.xml
 
