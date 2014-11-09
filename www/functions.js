fbdd4ed9068805902fa06cea86bd279b09745590
brueni <info@michael-bruenisholz.ch>
2014-11-09 17:10:34 +0100

    copied several other functions

--- www/functions.js
+++ www/functions.js
@@ -40,3 +40,58 @@ options = {
 
     series: []
 }
+
+// return everything after the question mark
+function GetUrlParameter() {
+    idx = window.location.href.indexOf("?");
+    if (idx < 0) return "";
+    return window.location.href.substring(idx + 1);
+}
+urlParameter = GetUrlParameter();
+
+function GetChartXml() {
+    switch (urlParameter) {
+    case "t3h":
+    case "t24h":
+    case "t48h":
+    case "t1w":
+    case "t1m":
+    case "t3m":
+    case "t1y":
+    case "h3h":
+    case "h24h":
+    case "h48h":
+    case "h1w":
+    case "h1m":
+    case "h3m":
+    case "h1y":
+        return "data/" + urlParameter + ".xml";
+    }
+    return "data/t24h.xml";
+}
+
+function GetChartTitle() {
+
+    var type = "Temperatures";
+    if (urlPath == "humid") {
+        type = "Humidity"
+    };
+    switch (urlParameter) {
+    case "3h":
+        return type + " of the last 3 hours";
+    case "48h":
+        return type + " of the last 48 hours";
+    case "1w":
+        return type + " of the last week";
+    case "1m":
+        return type + " of the last month";
+    case "3m":
+        return type + " of the last 3 month";
+    case "1y":
+        return type + " of the last year";
+    case "1yminmax":
+        return "Min-Max " + type + " of the last year";
+    }
+    return type + " of the last 24 hours";
+}
+;
