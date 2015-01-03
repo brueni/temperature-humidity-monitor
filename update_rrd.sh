e728866037a4b7830fbf0b56e66e5cccfb0d176f
brueni <info@michael-bruenisholz.ch>
2015-01-03 10:20:30 +0100

    changed output of current value to separate files

--- update_rrd.sh
+++ update_rrd.sh
@@ -7,5 +7,11 @@ output=$(python get_sensor_values.py 11 4)
 #execute RRD update
 $(rrdtool update data/temp-humid.rrd $output)
 
-echo $output > www/data/rack_current.txt
-echo $output
+#echo $output > www/data/rack_current.txt
+for i in $(echo $output | tr ":" "\n")
+do
+  values=("${values[@]}" "$i")
+done
+echo ${values[1]} > www/data/rack_t_current.txt
+echo ${values[2]} > www/data/rack_h_current.txt
+#echo $output
